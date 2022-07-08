82e0d98ebc9be14b009951a8f9d06a2d9264dcbe
temitopejegede <temiteejegede@gmail.com>
2022-07-08 18:59:50 -0400

    major refractoring using a DTO, added get book by id

--- src/main/java/com/temi/library/controllers/BookController.java
+++ src/main/java/com/temi/library/controllers/BookController.java
@@ -1,9 +1,14 @@
 package com.temi.library.controllers;
 
 import com.temi.library.model.Book;
+import com.temi.library.payload.BookDTO;
 import com.temi.library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/book")
@@ -14,8 +19,17 @@ public class BookController {
     private BookService bookService;
 
     @PostMapping("/add")
-    public String addBook(@RequestBody Book myBook){
-        bookService.saveBook(myBook);
-        return "New Book Added";
+    public ResponseEntity<BookDTO> addBook(@RequestBody BookDTO myBook){
+        return new ResponseEntity<>(bookService.saveBook(myBook), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getAllBooks")
+    public List<BookDTO> getAllBooks(){
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> getBookById(@PathVariable(name = "id") Long id){
+        return ResponseEntity.ok(bookService.getBookById(id));
     }
 }
