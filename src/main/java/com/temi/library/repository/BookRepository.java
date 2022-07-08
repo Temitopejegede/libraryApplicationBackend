d261db9dcdc5fb960625c597e9a1d7310e0dafde
temitopejegede <temiteejegede@gmail.com>
2022-07-08 12:54:59 -0400

    created repository that extends jpa repository

---
+++ src/main/java/com/temi/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.temi.library.repository;
+
+import com.temi.library.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
