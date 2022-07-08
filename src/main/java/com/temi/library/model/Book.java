b1bf46b49f95cc789a67a3e5790d171f5fa61197
temitopejegede <temiteejegede@gmail.com>
2022-07-07 23:59:21 -0400

    added columsn to data table

--- src/main/java/com/temi/library/model/Book.java
+++ src/main/java/com/temi/library/model/Book.java
@@ -2,6 +2,7 @@ package com.temi.library.model;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.Date;
@@ -16,14 +17,19 @@ public class Book {
     @Id
     private long id;
 
+    @Column(name = "title", nullable = false)
     private String title;
 
+    @Column(name = "Author", nullable = false)
     private String author;
 
+    @Column(name = "Number Of Pages", nullable = false)
     private int numberOfPages;
 
+    @Column(name = "Color", nullable = false)
     private String color;
 
+    @Column(name = "Date Added", nullable = false)
     private Date dateAdded;
 
 
