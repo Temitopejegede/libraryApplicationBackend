741be4d02a72e0289e8a64fa9855da24920490dd
temitopejegede <temiteejegede@gmail.com>
2022-07-08 16:06:24 -0400

    added to the book controller

--- src/main/java/com/temi/library/controllers/BookController.java
+++ src/main/java/com/temi/library/controllers/BookController.java
@@ -1,5 +1,21 @@
 package com.temi.library.controllers;
 
+import com.temi.library.model.Book;
+import com.temi.library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/book")
+@CrossOrigin
 public class BookController {
 
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping("/add")
+    public String addBook(@RequestBody Book myBook){
+        bookService.saveBook(myBook);
+        return "New Book Added";
+    }
 }
